e290db158a7dd566ee30ca228befcbfea43ed3eb
Yiyang Ling <lyy38706@sjtu.edu.cn>
2021-07-01 11:34:07 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ Simulator simulator;
 int main()
 {
 //    freopen("../sample/sample.data", "r", stdin);
-    freopen("../testcases/tak.data", "r", stdin);
+//    freopen("../testcases/tak.data", "r", stdin);
     mManager.init();
     std::cout << simulator.run() << std::endl;
 //    simulator.printReg();
